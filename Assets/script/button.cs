913c322c2f564a57d8a1041987a89792e828d2d7
sisyamo2236@gmail.com <sisyamo2236@gmail.com>
2021-02-09 00:19:35 +0900

    シーンの追加　素材の移動　ボタンの処理追加

---
+++ Assets/script/button.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button : MonoBehaviour
+{
+    //0:終了処理　1:タイトル画面　2:ゲーム画面　3:リザルト画面
+    [SerializeField] int scene;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStartButton()
+    {
+        switch (scene)
+            {
+            case 0:
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+
+                break;
+            case 1:
+                SceneManager.LoadScene("TITLE");
+
+                break;
+
+            case 2:
+
+                SceneManager.LoadScene("GAME");
+                break;
+
+            case 3:
+
+                SceneManager.LoadScene("RESULT");
+                break;
+        }
+        
+    }
+}
